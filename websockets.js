230c9d080f4ce6bb7951c2c1a04f130f2158b9ad
Adria <enti@rafamorning.com>
2020-02-18 20:29:23 +0100

    Agregado todos los mensajes de bala y puntiacion, faltan las mongo DB y el game over/ victoria.

--- websockets.js
+++ websockets.js
@@ -1,5 +1,8 @@
 var WSS = require('websocket').server;
 
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://192.168.122.42:27017/"
+
 var http = require('http');
 
 var web_server = http.createServer(function(req, res){
@@ -20,9 +23,10 @@ websocket_server.on('request', function(req){
 			p1 = req.accept(null, req.origin);
 			
 			p1.on('message', function(e){
-
+				if (JSON.parse(e.utf8Data).victor != null)
+					p1.send(e.utf8Data);	
+				
 				p2.send(e.utf8Data);	
-				console.log(e.utf8Data);
 			});
 
 			p1.send('{"player":1}');
@@ -30,6 +34,9 @@ websocket_server.on('request', function(req){
 	else if ( p2 == null){
 			p2 = req.accept(null, req.origin);
 			p2.on('message', function(e){
+				if (JSON.parse(e.utf8Data).victor != null)
+					p2.send(e.utf8Data);	
+
 				p1.send(e.utf8Data);
 			});
 
